cb38f9e6043a91bd6991d508c6e2654012fe611d
Thefaxepower <thefaxepower@gmail.com>
2020-04-21 12:33:39 +0200

    Finished RestController

--- src/main/java/be/ucll/taskmanager/controllers/RestController.java
+++ src/main/java/be/ucll/taskmanager/controllers/RestController.java
@@ -1,5 +1,34 @@
 package be.ucll.taskmanager.controllers;
 
+
+import be.ucll.taskmanager.models.TaskDTO;
+import be.ucll.taskmanager.services.TaskOverviewService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
 public class RestController {
-    
+
+    private final TaskOverviewService taskOverviewService;
+
+    public RestController(TaskOverviewService taskOverviewService) {
+        this.taskOverviewService = taskOverviewService;
+    }
+
+    @GetMapping("/tasks")
+    @ResponseBody
+    public List<TaskDTO> getTasks() {
+        return taskOverviewService.getTasks();
+    }
+
+    @PostMapping("/task")
+    public void createNewTask(@RequestBody @Valid TaskDTO taskDTO){
+        taskOverviewService.addTask(taskDTO);
+    }
+
 }
+
+
